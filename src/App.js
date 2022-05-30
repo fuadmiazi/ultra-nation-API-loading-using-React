48e886cd65c3b822d3a176dce065398aef52aec4
fuadmiazi <fuad.hasan.1997@gmail.com>
2022-05-30 10:41:45 +0600

    Cart functionality added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
   const [countries, setCountries] = useState([]);
-
+  const [cart, setCart] = useState([]);
   
   useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
@@ -13,13 +14,21 @@ function App() {
     .then(data=>setCountries(data)) 
     .catch(error => console.log(error))
   }, [])
+
+  const handleAddCountry = (country) => {
+    // console.log('added', country)
+    const newCart = [...cart, country];
+    setCart(newCart);
+  } 
   
    return (
     <div className="App">
       <h1>Country Loaded: {countries.length}</h1>
+      <h4>Country Added: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
       <ul>
         {
-          countries.map(country => <Country country={country} key={country.cca2} ></Country>)
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.cca2} ></Country>)
         }
       </ul>
       <header className="App-header">
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Country = (props) => {
     const {name, population, region, flags} = props.country;
-    //console.log(props);
+    const countryStyle = {border: '1px solid red', margin: '10px', padding: '10px'};
+    const handleAddCountry = props.handleAddCountry;
+
     return (
-        <div>
+        <div style={countryStyle}>
             <h4>This is {name.common}</h4>
             <img src={flags.png} width="200px" height="100px" alt="" />
             <p>Population: {population} </p>
             <p><small>Region: {region}</small></p>
-            
+            <button onClick={() => handleAddCountry(props.country)}>Add Country </button>
         </div>
     );
 };
